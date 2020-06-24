b2fbf7525e61426c130ed2a794ae77b9b8bd6b9a
Rairye <37942271+Rairye@users.noreply.github.com>
2020-06-24 10:04:34 +0900

    Update MeCabKeyWordFinder.py

--- MeCab/MeCabKeyWordFinder.py
+++ MeCab/MeCabKeyWordFinder.py
@@ -5,10 +5,16 @@ from collections import OrderedDict
 
 tagger = MeCab.Tagger()
 
-fileName = "MeCabWords.txt"
 lines = open(fileName, encoding='utf-8')
 
 word_counts = {}
+
+def update_counts(word):
+    if word in word_counts:
+        temp_count = word_counts[word]
+        word_counts[word] = temp_count + 1
+    else:
+        word_counts[word] = 1
 
 for line in lines:
     current_line = tagger.parse(line)
@@ -20,24 +26,19 @@ for line in lines:
         if current_word[1].startswith("名詞"):
             noun_components.append(current_word[0])
         else:
-            if len(noun_components) > 1:
-                compound = ''.join(noun_components)
-                if compound in word_counts:
-                    temp_count = word_counts[compound]
-                    word_counts[compound] = temp_count + 1
-                else:
-                    word_counts[compound] = 1
+            if len(noun_components) >= 1:
+                update_counts(''.join(noun_components))
             noun_components = []
                     
         if current_word[1].startswith("動詞") or (current_word[1].startswith("形容詞") or current_word[1].startswith("副詞")):
             word = current_word[0]
             if word in word_counts:
-                temp_count= word_counts[word]
-                word_counts[word] = temp_count + 1
-            else:
-                    word_counts[word] = 1
+                update_counts(word)
                     
         i+=1
+    
+    if len(noun_components) >= 1:
+        update_counts(''.join(noun_components))
 
 temp = OrderedDict(sorted(word_counts.items(), key=lambda t: t[1], reverse = True))
 results = temp
